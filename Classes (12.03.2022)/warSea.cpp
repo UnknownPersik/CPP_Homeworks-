ab12f8114f46d49c91c60ab501819ebc4782cad3
UnknownPersik <93385385+UnknownPersik@users.noreply.github.com>
2022-04-07 13:22:47 +0500

    Update warSea.cpp

--- Classes (12.03.2022)/warSea.cpp
+++ Classes (12.03.2022)/warSea.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <list>
 
+using namespace std;
+
+class Board {
+public:
+	Cell board[8][8];
+	void addShip(Ship x) {
+	}
+};
+
+class Cell {
+public:
+	int x;
+	int y;
+	bool isEmptyCell;
+};
+
+class Ship {
+public:
+	int length;
+	list <Cell> position;
+	bool isDead() {
+		return 0;
+	}
+};
+
+class Game {
+public:
+	bool Shoot(Cell c) {
+		return 0;
+	}
+
+};
+
+int main()
+{
+	return 0;
+}
