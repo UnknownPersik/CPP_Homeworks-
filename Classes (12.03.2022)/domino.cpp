0b9ce47812a682373087d7bdad37aabc88cf0b61
UnknownPersik <93385385+UnknownPersik@users.noreply.github.com>
2022-04-07 13:11:41 +0500

    Update domino.cpp

--- Classes (12.03.2022)/domino.cpp
+++ Classes (12.03.2022)/domino.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
 
+class dominoBone {
+public:
+	int leftSide;
+	int rightSide;
+	void putOnTheBoard(dominoBone bone, dominoBone &rightBone, dominoBone &leftBone, int locationOnBoard) {
+		if (locationOnBoard == 0) {
+			if (bone.rightSide == rightBone.leftSide) {
+				rightBone.leftSide = bone.leftSide;
+				rightBone.rightSide = bone.rightSide;
+			}
+			else {
+				rightBone.rightSide = bone.leftSide;
+				rightBone.leftSide = bone.rightSide;
+			}
+		}
+		else {
+			if (bone.rightSide == leftBone.leftSide) {
+				leftBone.leftSide = bone.leftSide;
+				leftBone.rightSide = bone.rightSide;
+			}
+			else {
+				leftBone.rightSide = bone.leftSide;
+				leftBone.leftSide = bone.rightSide;
+			}
+		}
+	}
+};
+
+int main()
+{
+	dominoBone left = { 3, 2 };
+	dominoBone right = { 5, 3 };
+	return 0;
+}
