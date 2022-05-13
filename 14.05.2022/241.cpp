a9d3ef37e7c81b258ecfe1181f3fc23f870655c4
UnknownPersik <93385385+UnknownPersik@users.noreply.github.com>
2022-05-13 23:10:46 +0500

    Update 241.cpp

--- 14.05.2022/241.cpp
+++ 14.05.2022/241.cpp
@@ -6,6 +6,13 @@ int k;
 
 int main(){
     cin >> k;
-    cout << 2 + k % 5 + 7 * (k / 5);
+    int result =  2 + k % 5 + 7 * (k / 5);
+    if (k > 36)
+        result++;
+    if (k > 44)
+        result++;
+    if (result == 56 || result == 63 || result == 70 || result == 71)
+        result += 2;
+    cout << result;
     return 0;
 }
