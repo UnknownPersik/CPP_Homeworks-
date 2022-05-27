896c7c863237a164fc9d907058ce30138ea97924
UnknownPersik <93385385+UnknownPersik@users.noreply.github.com>
2022-05-27 22:38:46 +0500

    Update 268.cpp

--- 28.05.2022/268.cpp
+++ 28.05.2022/268.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
 using namespace std;
@@ -7,15 +6,15 @@ using namespace std;
 void solve(){
     int n, k;
     cin >> n >> k;
-    string s;
-    cin >> s;
+    string str;
+    cin >> str;
     long long answer = n;
     for (int i = 0; i < n - 1; i++){
         int m = 0;
         int p = k;
-        while (i - m >= 0 && i + m + 1 < n && (p || s[i - m] == s[i + m + 1])){
+        while (i - m >= 0 && i + m + 1 < n && (p || str[i - m] == str[i + m + 1])){
             answer += 1;
-            if (s[i - m] != s[i + m + 1])
+            if (str[i - m] != str[i + m + 1])
                 p -= 1;
             m++;
         }
@@ -23,9 +22,9 @@ void solve(){
     for (int i = 0; i < n - 1; i++){
         int m = 1;
         int p = k;
-        while (i - m >= 0 && i + m < n && (p || s[i - m] == s[i + m])){
+        while (i - m >= 0 && i + m < n && (p || str[i - m] == str[i + m])){
             answer += 1;
-            if (s[i - m] != s[i + m])
+            if (str[i - m] != str[i + m])
                 p -= 1;
             m++;
         }
